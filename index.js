285bab76312ce6de3f05f87bdf2a9449cb12b932
Sourav <Souravbhardwaj997@gmail.com>
2025-06-24 17:36:03 +0530

    some minor changes

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.use(cookieParser());
 app.use("/api/user", userRoute); //Successfully working in postman
 app.use("/api/booking", authMiddleware, bookingRoute);
 app.use("/api/custom-booking", authMiddleware, customeBookingRoute);
-app.use("/api/payment", authMiddleware, paymentRoute);
+app.use("/api/payment", paymentRoute);
 
 //routes -> Admin
 app.use("/api/admin/meals", mealRoute); //Successfully Workin gin Postman
